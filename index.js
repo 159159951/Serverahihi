4251889095169bd7ade918591673a210cdca1504
Quang Huy <quanghuynguoiyeucuanguoiyeu@yahoo.com>
2016-06-08 15:01:35 +0700

    test demo server

--- index.js
+++ index.js
@@ -1,110 +1,68 @@
-// read file
-var readline = require('readline');
-var fileName = 'req-res_out.txt';
-var http = require('http')
-    , url = require('url')
-    , fs = require('fs')
-    , USERNAME = '/huyttq/'
-    , layout = '/layouts'
-    , device = '/accessories/0x0000'
-    , group = '/groups'
-    , preset = '/presets'
-    , event = '/events';
+// khai báo require module evilscan
+var evilscan = require('evilscan')
+// khai báo require module express
+,   express = require('express');
 
-var flgInit = true;
-var LineArr = fs.readFileSync(fileName)
-    .toString()
-    .split("\n");
+// kh?i t?o ?ng d?ng express
+var app = express();
 
-	
-function sleep(mili){
-
-	var start = new Date().getTime();
-	for(;;){
-		if((new Date().getTime() - start) > mili){
-			break;
-		}
-	}
-}
-var server = http.createServer(function (req, res) {
-/* 	console.log('\r\nDEBUG===\r\n');
-	console.log(req.url);
-	console.log(req.body);
-	console.log('\r\nEND DEBUG===\r\n'); */
-	 if (req.method == 'POST') {
-		console.log("Receive Req: Type = Post");
-        var body = '';
-
-        req.on('data', function (data) {
-            body += data;
-        });
-
-        req.on('end', function () {
-		    if(body == '{"pass":"a"}' ||body == 'pass=a' ){
-			console.log("++++");
-			console.log(body);
-				res.statusCode = 200;
-				sleep(1000);
-				res.end();
-			}
-			else{
-			console.log("----");
-			console.log(body);
-				res.statusCode = 404;
-				sleep(1000);
-				res.end();
-			}
-        });
-    }else if (req.url.indexOf(USERNAME) == -1) {
-		
-
- console.log("Receive Req: Type = GET");
-        if (flgInit) {
-            flgInit = false;
-            for (i in LineArr) {
-                console.log(LineArr[i]);
-            }
-            console.log("------------READ DATABASE SUCCESSFULL------------");
-        }
-        var str_Req = req.url;
-        urlSubReq = str_Req;
-        console.log('<<<<<' + urlSubReq);
-		sleep(1000);
-        res.writeHead(200, {
-            'Content-Type': 'application/json'
-        });
-
-        if (urlSubReq.indexOf('/0x') > -1) {
-            //Detail OR get all devices
-            console.log('Response for ' + urlSubReq);
-            // res detail
-            for (i = 0; i < LineArr.length; i++) {
-                if (LineArr[i].indexOf(urlSubReq) > -1) {
-                    console.log('Res line ' + (i + 2))
-                    console.log('>>>>>' + LineArr[i + 1]);
-                    res.write(LineArr[i + 1]);
-                    break;
-                }
-            }
-        } else {
-            // main page
-            console.log('Response for ' + urlSubReq);
-            for (i = 0; i < LineArr.length; i++) {
-                if ((LineArr[i].indexOf(urlSubReq) > -1) && (LineArr[i].indexOf('0x') == -1)) {
-                    console.log('Res line ' + (i + 2))
-                    console.log('>>>>>' + LineArr[i + 1]);
-                    res.write(LineArr[i + 1]);
-                    break;
-                }
-            }
-        }
-        res.end();
-
-    } else {
-        console.log('user fail');
-        res.statusCode = 404;
-        res.end();
-    }
+// ?ng d?ng port-scanning t? gi?i thi?u v? mình
+app.get('/', function(req, res){
+  res.send('<h1>Tui tên là port-scanning.<br>'+
+    'Tui ch?y r?t nhanh và r?t nguy hi?m.<br>'+
+    '<a href="http://vietjs.com/?p=9">http://vietjs.com/2014/06/01/quet-cong-mang-sieu-nhanh-su-dung-node-js/</a></h1>');
 });
-server.listen(80);
-console.log('Server running on port: 80');
+
+// ?ng d?ng port-scanning làm vi?c 
+app.get('/port/scan', function (req, res) {
+  var ips = req.query.ip
+  ,   ports = req.query.port
+  ,   options = {}
+  ,   results = [];
+
+  // ki?m tra l?i và tr? v? thông báo khi thi?u thông tin
+  if (!ips || !ports) {
+    res.send(400, 'Missing ip or port params. Correctly URL is /port/scan?ip=192.168.0.1&port=22');
+    return;
+  }
+
+  // khai báo options cho evilscan
+  // xem thông tin d?y d? t?i https://github.com/eviltik/evilscan
+  options = {
+    target: ips, // d?a ch? IP, dãy IP
+    port: ports, // c?ng
+    status:'TROU', // ?
+    banner: true, // hi?n th? bi?u ng? c?a c?ng k?t n?i
+    concurrency: 255, // s? lu?ng k?t n?i d?ng th?i
+    timeout: 2000, // th?i gian ch? k?t n?i (ms)
+    geo: true, // xác d?nh v? trí d?a lý c?a d?a ch? IP
+    reverse: true // hi?n th? thông tin reverse dns
+  };;
+
+  // kh?i t?o evilscan scanner
+  var scanner = new evilscan(options, function () {
+    // kh?i t?o xong scanner
+  });
+    
+  // khi quét có k?t qu?, luu k?t qu? vào bi?n results
+  scanner.on('result', function (data) {
+    results.push(data);
+  });
+
+  // khi có l?i, tr? v? thông báo l?i
+  scanner.on('error', function (err) {
+    res.send(500, 'evilscan error: ' + err);
+  });
+
+  // khi quét xong, k?t thúc x? lý cho truy v?n này và tr? v? k?t qu?
+  scanner.on('done', function () {
+    res.send(results);
+  });
+
+  // ch?y evilscan scanner
+  scanner.run();
+});
+
+var HTTP_PORT = process.env.PORT || 3000;
+app.listen(HTTP_PORT);
+console.log('port-scanning application listening at 0.0.0.0:' + HTTP_PORT);
